94de1c23dcd76762b43724b99dcbb56aca276eda
leocuesp-prog <leocuesp@gmail.com>
2026-08-25 16:20:30 -0500

    Remove GEMINI_API_KEY definition
    Removed the API key definition for security reasons.

--- minovita/gemini_key.php
+++ minovita/gemini_key.php
@@ -3,4 +3,4 @@
 Llave de api key (llave de gemini para que funcione minovita)
 ════════════════════════════════════════════════════════ */
 
-define('GEMINI_API_KEY', 'AQ.Ab8RN6LpYc5Ad52WdfvamvEHe6CONA_IINcYg0WJJhztyWNsyg');
+// lol xdxdxdxd
